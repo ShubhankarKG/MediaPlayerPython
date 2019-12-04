1c0a68e9bde9f3a1d9d54a14056b61b28d191c0f
Harsimran <harsimranvirk99@gmail.com>
2019-12-04 23:23:26 +0530

    Added play, pause and volume control functionality. Added a test.mp3 file.

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from tkinter import *
 from pygame import mixer
 
+
 if __name__ == "__main__":
+    isPaused = False
+    def play_music():
+        global isPaused
+        if isPaused:
+            isPaused = False
+            mixer.music.unpause()
+        else:
+            mixer.music.load("test.mp3")
+            mixer.music.play()
+    def pause_music():
+        global isPaused
+        isPaused = True
+        mixer.music.pause()
+    def vol_control(vol):
+        volume = float(vol)/100
+        mixer.music.set_volume(volume)
+
 
     # BASIC TKINTER CODE
     m1 = PanedWindow()
@@ -11,6 +29,11 @@ if __name__ == "__main__":
     m1.add(left)
     m2 = PanedWindow(m1, orient = VERTICAL)
     m1.add(m2)
-    top = Scale(m2, orient = HORIZONTAL)
+    top = Scale(m2, orient = HORIZONTAL, command = vol_control)
+    top.set(100)
     m2.add(top)
+    play = Button(m2, text = "Play", command = play_music)
+    m2.add(play)
+    pause = Button(m2, text = "Pause", command = pause_music)
+    m2.add(pause)
     mainloop()
