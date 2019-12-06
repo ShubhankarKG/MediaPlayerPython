d0668098edca1c4c726becbdeef400ebff19ca14
upcomingdeveloper <46356490+upcomingdeveloper@users.noreply.github.com>
2019-12-06 13:11:03 +0530

    Added timer, fixed design

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from tkinter import *
 import tkinter.filedialog
 from pygame import mixer
+from mutagen.mp3 import MP3
+import time
+import threading
 
 
 if __name__ == "__main__":
@@ -9,6 +12,7 @@ if __name__ == "__main__":
 
     def play_music():
         global isPaused
+        global song_len
         if isPaused:
             isPaused = False
             mixer.music.unpause()
@@ -20,6 +24,8 @@ if __name__ == "__main__":
             else:
                 mixer.music.load(file_name)
                 mixer.music.play()
+                thread = threading.Thread(target=timer, args=(song_len,))
+                thread.start()
 
     def pause_music():
         global isPaused
@@ -33,11 +39,20 @@ if __name__ == "__main__":
 
     def search_file():
         global file_name
+        global song_len
         file_name = tkinter.filedialog.askopenfilename()
-        song_name['text'] = file_name.split('/')[-1] #Works for linux, but not for Windows
+        song_name['text'] = file_name.split('/')[-1] #Works for linux and mac, but not for Windows
+        song_len = MP3(file_name).info.length
+        total_time["text"] = time.strftime("%M:%S", time.gmtime(song_len))
+        #timer(song_len)
         if mixer.music.get_busy():
             mixer.music.stop()
 
+    def timer(length):
+        while(length):
+            current_time["text"] = time.strftime("%M:%S", time.gmtime(length))
+            length -= 1
+            time.sleep(1)
 
     # BASIC TKINTER CODE
     m1 = PanedWindow()
@@ -46,9 +61,11 @@ if __name__ == "__main__":
     frame = LabelFrame(m1, bd=5)
     m1.add(frame)
     song_name = Message(frame, width=300)
-    song_name.grid(row=0)
-    time = Label(frame, width=40)
-    time.grid(row=1)
+    song_name.grid(row=0,columnspan=2)
+    total_time = Label(frame, width=20)
+    total_time.grid(row=1, column=0)
+    current_time = Label(frame,width=20)
+    current_time.grid(row=1, column=1)
     m2 = PanedWindow(m1, orient=VERTICAL)
     m1.add(m2)
     top = Scale(m2, orient=HORIZONTAL, command=vol_control)
